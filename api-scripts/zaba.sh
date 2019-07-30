84f2b520ebdb5ab0257050025093c395786ae724
asdfMaciej <maciej.kaszkowiak@gmail.com>
2019-07-30 14:12:42 +0200

    we no longer need the static file in the main directory

--- api-scripts/zaba.sh
+++ api-scripts/zaba.sh
@@ -3,6 +3,6 @@ cd /root/
 python3 promki.py
 python3 html.py
 python3 phper.py
-mv index.html /www/zabka/
+mv index.html /www/zabka-static/
 mv promotions.php /www/api/
 cp promocje.csv /root/backups/`date +%s`
